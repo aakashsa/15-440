5ccf64570e1f70bdfc03509d1d2f9067f110eb2b
Nikhil Tibrewal <1803.nikhi@gmail.com>
2013-01-22 18:20:32 -0500

    scanner finished

--- project1/src/scanner.java
+++ project1/src/scanner.java
@@ -1,43 +1,53 @@
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 
 public class scanner {
 
 	/**
 	 * @param args
 	 */
-		    public static void main(String[] args) throws IOException {
-		    	
-	        	System.out.print("\n Reading Lines \n");
+	public static void main(String[] args) throws IOException {
 
-	            ArrayList<String> myArr = new ArrayList<String>();
-	            int i =0;
-	        	Scanner sc2 = null;
-		    	sc2 = new Scanner(System.in);
-		    	while (sc2.hasNextLine()) {
-		    		myArr.clear();
-	    	    	System.out.print("\n Line  : " +i +"\n");
+		System.out.print("\n Reading Lines \n");
 
-		    	    Scanner s2 = new Scanner(sc2.nextLine());
-		    	    boolean b;
-		    	    while (b = s2.hasNext()) {
-		    	    	System.out.print("\n Read Word :  \n");
-		    	        String s = s2.next();
-		    	        myArr.add(s);
-		            	System.out.print(myArr.get(myArr.size()-1));
-		    	    }
-		    	    System.out.println("myArray : ");
-		    	    System.out.print(myArr.toString());
-		    	    System.out.println("\n ");
-		    	    i++;
-		    	    System.out.println("nikhil's change");
-		    	}
-		    }
+		ArrayList<String> myArr = new ArrayList<String>();
+
+		Scanner sc2 = new Scanner(System.in);
+
+		while (sc2.hasNextLine()) {
+			myArr.clear();
+			Scanner s2 = new Scanner(sc2.nextLine());
+			boolean b;
+
+			// Scan the first word for process name or a different command
+			if (s2.hasNext()) {
+				String name = s2.next();
+				if (name.equals("ps")) {
+					if (s2.hasNext()) {
+						System.out
+								.println("Invalid command: ps does not take any arguments!");
+					} else {
+						System.out.println("Display all processes");
+					}
+				} else if (name.equals("quit")) {
+					if (s2.hasNext()) {
+						System.out
+								.println("Invalid commant: quit does not take any arguments!");
+					} else {
+						System.out.println("Quit prorgram");
+						break;
+					}
+				} else {
+					// need to start a new process; parse arguments
+					while (b = s2.hasNext()) {
+						myArr.add(s2.next());
+					}
+					System.out
+							.println("Started new process with following information: name: "
+									+ name + ", arguments: " + myArr.toString());
+				}
+			}
+		}
+	}
 }
-		
-
-
-	
