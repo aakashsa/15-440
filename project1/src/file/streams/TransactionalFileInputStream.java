2388f09311cdadccade70235b1a92911c2d8232c
Aakash Sabharwal <aakashsa@andrew.cmu.edu>
2013-01-21 22:10:36 -0500

    Added Files

---
+++ project1/src/file/streams/TransactionalFileInputStream.java
@@ -0,0 +1,19 @@
+package file.streams;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.io.OutputStream;
+
+public class TransactionalFileInputStream   extends OutputStream implements Serializable {
+
+	public TransactionalFileInputStream(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void write(int arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ project1/src/file/streams/TransactionalFileOutputStream.java
@@ -0,0 +1,21 @@
+package file.streams;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+
+public class TransactionalFileOutputStream extends InputStream  implements Serializable {
+
+	public TransactionalFileOutputStream(String string, boolean b) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	@Override
+	public int read() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
