4e05e7b974dd9bc06cc465fce4c23b1078402db4
Aakash Sabharwal <aakashsa@andrew.cmu.edu>
2013-01-21 23:44:56 -0500

    File Streams

--- project1/src/file/streams/TransactionalFileInputStream.java
+++ project1/src/file/streams/TransactionalFileInputStream.java
@@ -1,19 +1,46 @@
 package file.streams;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
-import java.io.OutputStream;
 
-public class TransactionalFileInputStream   extends OutputStream implements Serializable {
+public class TransactionalFileInputStream   extends FileInputStream implements Serializable {
 
-	public TransactionalFileInputStream(String string) {
-		// TODO Auto-generated constructor stub
+	private long pointer;
+	
+	public TransactionalFileInputStream(String string) throws FileNotFoundException {
+		super(string);
+		pointer = 0;
 	}
 
 	@Override
-	public void write(int arg0) throws IOException {
-		// TODO Auto-generated method stub
-		
+	public int read() throws IOException {
+		super.skip(pointer);
+		byte[] readByte = new byte[1]; 
+		int returnValue = super.read(readByte,0, 1);
+		if (returnValue!=-1)
+			pointer+=returnValue;
+		return readByte[0];
 	}
-
+	
+	public int read(byte[] b,int off,int len) throws IOException {
+		
+		super.skip(pointer);
+		int returnValue = super.read(b,off, len);
+		if (returnValue!=-1)
+			pointer+=returnValue;
+		return returnValue;
+	}
+	
+	 
+	public int read(byte[] b) throws IOException {
+		
+		super.skip(pointer);
+		int returnValue = super.read(b);
+		if (returnValue!=-1)
+			pointer+=returnValue;
+		return returnValue;
+	}
 }
--- project1/src/file/streams/TransactionalFileOutputStream.java
+++ project1/src/file/streams/TransactionalFileOutputStream.java
@@ -1,21 +1,26 @@
 package file.streams;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 
-public class TransactionalFileOutputStream extends InputStream  implements Serializable {
+public class TransactionalFileOutputStream extends OutputStream  implements Serializable {
 
 	public TransactionalFileOutputStream(String string, boolean b) {
+		
 		// TODO Auto-generated constructor stub
 	}
 
-
-
+	
 	@Override
-	public int read() throws IOException {
+	public void write(int b) throws IOException {
 		// TODO Auto-generated method stub
-		return 0;
+		byte lowByte = (byte)(b & 0xFF);
+		this.write(lowByte);
 	}
+
+
+		
+	
 
 }
