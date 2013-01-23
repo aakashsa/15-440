67fa27f23a863a2a275624c2f0d1620fa263fbdd
Aakash Sabharwal <aakashsa@andrew.cmu.edu>
2013-01-23 13:09:38 -0500

    PM first Commit

--- project1/src/processManager.java
+++ project1/src/processManager.java
@@ -22,3 +22,51 @@ public class processManager {
 			
 	}	
 }
+ /* 
+	
+	// TODO Auto-generated method stub
+	try {
+		Class c = Class.forName("processes.GrepProcess");
+		System.out.println(" Class Name = " +c.getName() + " \n");
+
+		Class[] ctorArgs1 = new Class[1];
+
+		ctorArgs1[0] = String[].class;
+        Constructor strCtor = null;
+		try {
+			strCtor = c.getConstructor(ctorArgs1);
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+            System.out.println("Constructor found: " +
+                strCtor.toString() +"\n");
+            String [] hello = new String[3];
+            hello[0] = "hewyy";
+            hello[1] = "hdhaj";
+            hello[2] =" yfabjdbsja";
+            try {
+				System.out.println(" New Instance = " + strCtor.newInstance((Object) hello).toString() );
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+           
+	} catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+	*/
