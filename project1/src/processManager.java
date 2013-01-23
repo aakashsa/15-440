3ed09596a3ef93f8d0544352a5b45bb2bb0e27f1
Aakash Sabharwal <aakashsa@andrew.cmu.edu>
2013-01-23 12:57:03 -0500

    PM first Commit

---
+++ project1/src/processManager.java
@@ -0,0 +1,24 @@
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class processManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (!args[0].equals("processManager")){
+			System.out.println("Invalid Command");
+		}
+		if (args[1].equals("-c")){
+			// Slave Case
+		}
+		else {
+			//Master Case
+		}
+			
+			
+			
+	}	
+}
