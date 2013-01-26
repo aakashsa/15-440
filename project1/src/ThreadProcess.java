30d4e88db5fb7ce278318f99f1fce2f25dd7f238
Nikhil Tibrewal <1803.nikhi@gmail.com>
2013-01-26 15:39:18 -0500

    threadProcess class added

---
+++ project1/src/ThreadProcess.java
@@ -0,0 +1,29 @@
+import interfaces.MigratableProcess;
+
+
+public class ThreadProcess {
+	
+	private MigratableProcess p;
+	private Thread t;
+	
+	public ThreadProcess(Thread t, MigratableProcess p) {
+		this.t = t;
+		this.p = p;
+	}
+	
+	public Thread getThread() {
+		return t;
+	}
+	
+	public MigratableProcess getProcess() {
+		return p;
+	}
+	
+	public boolean threadIsAlive() {
+		return t.isAlive();
+	}
+	
+	public void assignNewThread(Thread t2) {
+		this.t = t2;
+	}
+}
